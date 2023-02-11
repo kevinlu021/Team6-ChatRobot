82d9e130f4ea36b624b0646c3ee54a7325c49f45
kevl0317 <kevin.liu1270@gmail.com>
2023-02-10 18:38:26 -0800

    Update Voice Recognition

---
+++ voice_recognition.py
@@ -0,0 +1,100 @@
+
+import sys
+import queue
+import argparse
+import sounddevice as sd
+import json
+from vosk import Model, KaldiRecognizer, SetLogLevel
+
+SetLogLevel(-1)
+
+chinese_model_path = ".\model\CN\model.pth"
+chinese_config_path = ".\model\CN\config.json"
+japanese_model_path = ".\model\H_excluded.pth"
+japanese_config_path = ".\model\config.json"
+
+q = queue.Queue()
+def int_or_str(text):
+    """Helper function for argument parsing."""
+    try:
+        return int(text)
+    except ValueError:
+        return text
+
+
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
+
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument(
+    "-l", "--list-devices", action="store_true",
+    help="show list of audio devices and exit")
+args, remaining = parser.parse_known_args()
+if args.list_devices:
+    parser.exit(0)
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[parser])
+parser.add_argument(
+    "-f", "--filename", type=str, metavar="FILENAME",
+    help="audio file to store recording to")
+parser.add_argument(
+    "-d", "--device", type=int_or_str,
+    help="input device (numeric ID or substring)")
+parser.add_argument(
+    "-r", "--samplerate", type=int, help="sampling rate")
+parser.add_argument(
+    "-m", "--model", type=str, help="language model; e.g. en-us, fr, nl; default is en-us")
+args = parser.parse_args(remaining)
+try:
+    if args.samplerate is None:
+        device_info = sd.query_devices(args.device, "input")
+        # soundfile expects an int, sounddevice provides a float:
+        args.samplerate = int(device_info["default_samplerate"])
+
+    if args.model is None:
+        model = Model(lang="en-us")
+    else:
+        model = Model(lang=args.model)
+
+    if args.filename:
+        dump_fn = open(args.filename, "wb")
+    else:
+        dump_fn = None
+
+
+
+except KeyboardInterrupt:
+    print("\nDone")
+    parser.exit(0)
+
+def voice_input():
+    model = Model(lang="en-us")
+    print("You:")
+    with sd.RawInputStream(samplerate=args.samplerate, blocksize=8000, device=args.device,
+                           dtype="int16", channels=1, callback=callback):
+
+        rec = KaldiRecognizer(model, args.samplerate)
+        while True:
+            data = q.get()
+            if rec.AcceptWaveform(data):
+                a = json.loads(rec.Result())
+                a = str(a['text'])
+                a = ' '.join(a.split())
+                if(len(a) > 0):
+                    print(a)
+                    user_input = a
+                    return user_input
+            if dump_fn is not None:
+                dump_fn.write(data)
+            
+
+
+                
+if __name__ == "__main__":
+    resp = voice_input()
